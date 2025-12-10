12d68a7b8a0271935e7e3cbe59876f2a2c4a515f
Nikos.p <connor2000@hotmail.gr>
2025-12-10 12:32:27 +0200

    feat: add bank-app inside ch14 & user defined exceptions

---
+++ src/gr/aueb/cf/cf9/ch14/bank_app/NegativeAmountException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.ch14.bank_app;
+
+
+public class NegativeAmountException extends Exception {
+
+    public NegativeAmountException(String message) {
+        super(message);
+    }
+}
