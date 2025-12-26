342626dca72a81b8052fc29bb27ee0ef0ab64136
Nikos.p <connor2000@hotmail.gr>
2025-12-26 20:16:38 +0200

    feat: add Acount CRUD Contract (interface)

---
+++ src/gr/aueb/cf/cf9/ch18/bankapp/DAO/IAccountDAO.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.cf9.ch18.bankapp.DAO;
+
+import gr.aueb.cf.cf9.ch18.bankapp.model.Account;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IAccountDAO {
+    Account saveOrUpdate(Account account);
+
+    void remove(String iban);
+
+    Optional<Account> findByIban(String iban);
+
+    List<Account> getAllAccounts();
+
+    // Queries
+    boolean isAccountExists(String iban);
+}
